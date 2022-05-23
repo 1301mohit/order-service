20c251f65d74fb0df5dc15d8cad3a424602aa48e
Mohit Kumar <mohkumar26@publicisgroupe.net>
2022-05-23 12:26:22 +0530

    Added eureka service client

--- src/main/java/com/java/orderservice/api/service/OrderService.java
+++ src/main/java/com/java/orderservice/api/service/OrderService.java
@@ -26,7 +26,7 @@ public class OrderService {
 		payment.setOrderId(order.getId());
 		payment.setAmount(order.getPrice());
 		//payment api call
-		Payment paymentResponse = template.postForObject("http://localhost:9191/payment/doPayment", payment, Payment.class);
+		Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
 		response = paymentResponse.getPaymentStatus().equals("success")?"Payment processing successful and order placed":"there is a failure in payment api, order added to cart";
 		repository.save(order);
 		return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
